35e7c2f70cc472b85cb3d86153078789cce6f1ff
CarisAvi21 <shinnytinystar@hotmail.com>
2018-07-20 09:22:30 -0500

    Cipher Actualizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,43 +1,49 @@
 window.cipher = {
-  /*encode: (usuario, clave) => {
-    let cifrarTexto = "";
-    let asciiFormula = 0;
-    for (let i= 0; i < usuario.length; i++) {
-      if (usuario.charCodeAt(i) >= 65 && usuario.charCodeAt(i) <= 90) {
-        asciiFormula = (usuario.charCodeAt(i) - 65 + 33) % 26 + 65 ;
-        //cifrarTexto += String.fromCharCode(asciiFormula);
-    } else if (usuario.charCodeAt(i) >= 97 && usuario.charCodeAt(i) <= 122) {
-      asciiFormula = (usuario.charCodeAt(i) - 97 + 33) % 26 + 97 ;
-      //cifrarTexto += String.fromCharCode(asciiFormula);
-    } else if (usuario.charCodeAt(i) >= 32 && usuario.charCodeAt(i) <= 64){
-      asciiFormula = (usuario.charCodeAt(i) - 32  + 33) % 26 + 32 ;
-      //cifrarTexto += String.fromCharCode(asciiFormula);
+  encode: (offset, string) => {
+    let txtcifrado = '';
+
+    // for es un bucle que se va a repetir un numero especifico de veces, en este caso va a recorrer el texto que introduzaca el usuario para poder cifrarlo 
+    for (let i = 0; i < string.length; i++) {
+      // ascii es el código que utilizan las computadoras que representas símbolos, caracteres y números, en este caso el código ascii va a empezar en el #65 del alfabeto.
+      let asciiFormula = string[i].charCodeAt(); // aqui se regresa el numero ascii y lo devuelve en la variable asciiFormula
+      let formula;
+      // sie el usuario escribe un texto con mayusculas, hara esta formula,
+      if (asciiFormula >= 65 && asciiFormula <= 90) {
+        // parseInt se encarga de analizar una cadena e intentar obtener un valor numerico a partir de esta, en este caso analizara la clave que ponga el usuario para poder cifrar.
+        formula = (asciiFormula - 65 + parseInt(offset)) % 26 + 65;
+        // si ademas ocupa minusculas aplicara esta formula
+      } else if (asciiFormula >= 97 && asciiFormula <= 122) {
+        formula = (asciiFormula - 97 + parseInt(offset)) % 26 + 97;
+        // si utiliza espacios, aplicara esta formula
+      } else formula = asciiFormula;
+
+      // // charCodeAt: Devuelve el valor Unicode del carácter en la ubicación especificada
+      // unicode: es un formato común de caracteres
+      // en este caso devolvera un el string cifrado con el codigo ascii
+      txtcifrado += String.fromCharCode(formula);
     }
- cifrarTexto += String.fromCharCode(asciiFormula);
-  }
-    return document.getElementById('texto2').value = cifrarTexto
-    console.log('cifrarTexto');
-},
-decode: (usuario2, clave) =>{
-  let descifrarTexto = "";
-  let acsiiFormula = 0;
-  for (let i= 0; i < usuario2.length; i++) {
-    if (usuario2.charCodeAt(i) >= 65 && usuario2.charCodeAt(i) <= 90) {
-      asciiFormula = (usuario2.charCodeAt(i) - 90 - 33) % 26 + 90;
-      //descifrarTexto += String.fromCharCode(asciiFormula)
-//si no son mayusculas se aplica esto para convertirse en minusculas
-} else if (usuario2.charCodeAt(i) >= 97 && usuario2.charCodeAt(i) <= 122) {
-    asciiFormula = (usuario2.charCodeAt(i) - 122 - 33) % 26 + 122;
-    //descifrarTexto += String.fromCharCode(asciiFormula)
-//se aplica cuando el usuario utiliza espacios o caracteres especiale
-} else if (usuario2.charCodeAt(i) >= 32 && usuario2.charCodeAt(i) <= 64) {
-    asciiFormula = (usuario2.charCodeAt(i) - 32 - 33) % 26 + 32;
-    //descifrarTexto += String.fromCharCode(asciiFormula)
-   }
- descifrarTexto += String.fromCharCode(asciiFormula);
- }
- console.log(descifrarTexto);
- return document.getElementById('txt3').value = descifrarTexto;
-},*/
-// ...
+    return txtcifrado;
+
+  },
+
+  decode: (offset2, stringp) => {
+    let txtdescifrado = '';
+
+    for (let i = 0; i < stringp.length; i++) {
+      let asciiFormula = stringp[i].charCodeAt();
+      let formula;
+
+      if (asciiFormula >= 65 && asciiFormula <= 90) {
+        formula = (asciiFormula - 90 - parseInt(offset2)) % 26 + 90;
+      } else if (asciiFormula >= 97 && asciiFormula <= 122) {
+        formula = (asciiFormula - 122 - parseInt(offset2)) % 26 + 122;
+      } else formula = asciiFormula;
+
+      txtdescifrado += String.fromCharCode(formula);
+    }
+
+    return txtdescifrado;
+  },
+  
+  createCipherWithOffset: () =>{},
 };
--- src/index.js
+++ src/index.js
@@ -1,79 +1,25 @@
-//aqui se encriptara el mensaje que introduzca el usuario
- const cifrar = () =>  {
-   //console.log("INICIO DE FUNCION");
-  let usuario = document.getElementById('texto').value;
-  let cifrarTexto = "";
-  let asciiFormula = 0;
-  let clave= 33;
-//  let encode = cipher.encode(cifrarTexto, clave)
+let string = document.getElementById('texto');
+let botonCifrar = document.getElementById('encode');
+let offset = document.getElementById('numero');
+let texto2 = document.getElementById('texto2');
+let stringp = document.getElementById('texto3');
+let botonDescifrar = document.getElementById('decode');
+let offset2 = document.getElementById('numero2');
+let txt3 = document.getElementById('txt3');
 
-//se agrega un loop para que comienza a hacer el cifrado de texto
-  for (let i= 0; i < usuario.length; i++) {
-    //asciiFormula = (usuario.charCodeAt(i) - 65 + 33) % 26 + 65 ;//FORMULA PARA cifrar EN ASCII
-    //cifrarTexto += String.fromCharCode(asciiFormula);
+// se la da un evento al boton, para que realice la acción del "OnClock" y realice la funcion de cifrado y la pinte ne pantalla 
+botonCifrar.addEventListener('click', event => {
+  
+  let clickEncode = cipher.encode(offset.value, string.value);
 
-//si el usuario utiliza solo MAYUSCULA se aplica est aformula
-      if (usuario.charCodeAt(i) >= 65 && usuario.charCodeAt(i) <= 90) {
-        asciiFormula = (usuario.charCodeAt(i) - 65 + 33) % 26 + 65 ;
-        //cifrarTexto += String.fromCharCode(asciiFormula);
+  texto2.value = clickEncode;
+ 
+  
+});
 
-
-//si el usuario utiliza MINUSCULAS  se salta a esta formula
-    } else if (usuario.charCodeAt(i) >= 97 && usuario.charCodeAt(i) <= 122) {
-      asciiFormula = (usuario.charCodeAt(i) - 97 + 33) % 26 + 97 ;
-      //cifrarTexto += String.fromCharCode(asciiFormula);
-
-
-//se aplica cuando el usuario utiliza espacios o caracteres especiales
-    } else if (usuario.charCodeAt(i) >= 32 && usuario.charCodeAt(i) <= 64){
-      asciiFormula = (usuario.charCodeAt(i) - 32  + 33) % 26 + 32 ;
-      //cifrarTexto += String.fromCharCode(asciiFormula);
-    }
- cifrarTexto += String.fromCharCode(asciiFormula);
-
-
-}
-//nos imprime el texto cifrado
-    return document.getElementById('texto2').value = cifrarTexto
-    console.log('cifrarTexto');
-}
-
-//aqui se comenzara a descifrar el texto cifrado arriba
-const descifrar = () => {
-  let descifrarTexto = "";
-  let usuario2 = document.getElementById('texto3').value;
-  let acsiiFormula = 0;
-  let clave = 33;
-  //let decode = cipher.decode(descifrarTexto, clave)
-
-  //SE AGREGA UN LOOP PARA EMPEZAR A DESCIFRAR EL TEXTO QUE EL USUARIO QUIERA INGRESAR
-  for (let i= 0; i < usuario2.length; i++) {
-  //  asciiFormula = (usuario2.charCodeAt(i) + 65 - 33) % 26 + 65; //formula para descifrar en ascii
-    //descifrarTexto += String.fromCharCode(asciiFormula)
-
-
-//el texto cifrado en mayusculas se descifra con esta formula
-     if (usuario2.charCodeAt(i) >= 65 && usuario2.charCodeAt(i) <= 90) {
-       asciiFormula = (usuario2.charCodeAt(i) - 90 - 33) % 26 + 90;
-       //descifrarTexto += String.fromCharCode(asciiFormula)
-
-
-//si el texto cifrado no son mayusculas se aplica esto para convertirse en minusculas
-} else if (usuario2.charCodeAt(i) >= 97 && usuario2.charCodeAt(i) <= 122) {
-     asciiFormula = (usuario2.charCodeAt(i) - 122 - 33) % 26 + 122;
-     //descifrarTexto += String.fromCharCode(asciiFormula)
-
-
-//se aplica cuando el usuario utiliza espacios o caracteres especiales
-} else if (usuario2.charCodeAt(i) >= 32 && usuario2.charCodeAt(i) <= 64) {
-     asciiFormula = (usuario2.charCodeAt(i) - 32 - 33) % 26 + 32;
-     //descifrarTexto += String.fromCharCode(asciiFormula)
-
-    }
-  descifrarTexto += String.fromCharCode(asciiFormula);
-}
-  console.log(descifrarTexto);
-  //nos imprime el texto descifrado
-  return document.getElementById('txt3').value = descifrarTexto;
-
-}
+botonDescifrar.addEventListener('click', event => {
+  let clickDecode = window.cipher.decode(
+    offset2.value, stringp.value
+  );
+  txt3.value = clickDecode;
+});
